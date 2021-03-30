b2909bca60e4acf9798933701b447a436384634f
SantosPereira <pedrohenriquelemam@gmail.com>
2021-03-30 10:53:39 -0300

    Reformulação do código

--- neurônio.py
+++ neurônio.py
@@ -1,9 +1,9 @@
 '''
 README:
 
-O esquema básico de uma rede neural é ter uma entrada de dados, dar pesos a cada grupo de dados e faze-los passar pelos neurônios.
+O esquema básico de uma rede neural é ter uma entrada de dados, dar pesos a cada grupo de dados e fazê-los passar pelos neurônios.
 Os nerônios são portões lógicos, de acordo com o que são programados vão testar condições lógicas como if, else, ==, !=, >, <, =>, =<.
-E vão dar como saída outros valores, como True, False, 0, 1, ou quaser outro valor.
+E vão dar como saída outros valores, como True, False, 0, 1, ou qualquer outro valor.
 '''
 '''
 Nesse link tem um exemplo:
@@ -12,32 +12,32 @@ https://www.youtube.com/watch?v=NZlIYr1slAk&t=50s
 
 ###         O exemplo abaixo controla se um carro vai para a direita ou esquerda e se acelera
 
+from time import sleep
 
 ### entrada de dados
-in1 = 10 #in == input data
-in2 = 15
-in3 = 20
 
-##################### pesos
-in1 = in1*1.1
-in2 = in2*1.5
-in3 = in3*1.7
+direita = 10 # distância até a parede direita
+esquerda = 15 # distância até a parede esquerda
+frente = 20 # distância até a parede
 
-################# neurônios
-def tico():
-    if in1 >= 11:
+right = False
+left = False
+xlr8 = False
+
+################# neurônio
+while True:
+    sleep(0.25)
+    if frente < 3:
         right = True
+        xlr8 = True
     else:
-        right = False
-
-    if in2 >= 15:
         left = True
-    else:
-        left = False
+        xlr8 = True
+    print(right)
+    print(left)
+    print(xlr8)
+        
+    right = False
+    left = False
+    xlr8 = False
 
-    if in3 >= 27:
-        xlr8 = True #acelerate
-    else:
-        xlr8 = False
-def teco():
-    pass
